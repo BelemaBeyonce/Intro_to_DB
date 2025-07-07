a626420e89d12718d01c1f3cc545e1f2a871399e
Belema_adim <adimbelema205@gmail.com>
2025-07-07 03:00:23 +0100

    Adding the file

--- task_6.sql
+++ task_6.sql
@@ -2,12 +2,14 @@ USE alx_book_store;
 
 INSERT INTO customer (customer_id, customer_name, email, address)
 VALUES
-(customer_id=2, customer_name="Blessing Malik", email="bmalik@sandtech.com", address="124 Happiness Ave."),
+(customer_id=2, customer_name="Blessing Malik", email="bmalik@sandtech.com", address="124 Happiness  Ave."),
 (customer_id=3, customer_name="Obed Ehoneah", email="eobed@sandtech.com", address="125 Happiness  Ave."),
 (customer_id=4, customer_name="Nehemial Kamolu", email="nkamolu@sandtech.com", address="126 Happiness  Ave.");
 
 delete from customer
 where customer_id > 1;
+
+
 
 
 
